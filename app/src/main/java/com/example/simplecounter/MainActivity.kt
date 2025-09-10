67667e16bf04e5394ba9e80d3b5848680ebd4327
JuliaLam <julialam2510@gmail.com>
2025-09-10 11:52:21 -0400

    Added stretch goal

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.simplecounter
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,11 +23,31 @@ class MainActivity : AppCompatActivity() {
         }
         val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
+        val upgradeButton = findViewById<Button>(R.id.upgradeBtn)
+
         button.setOnClickListener {
             //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
             counter++
             textView.text=counter.toString()
+
+            if (counter >= 100) {
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 2"
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgradeButton.visibility = View.INVISIBLE
+            }
+            }
         }
+
+
 
 
     }
